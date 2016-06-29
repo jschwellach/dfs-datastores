af75ef0d6c2817ef639e5dfa98f6db80a5893961
Janos Schwellach <janos.schwellach@helixleisure.com>
2016-06-29 16:50:02 +0800

    EMRFS Workaround

--- dfs-datastores/src/main/java/com/backtype/hadoop/Consolidator.java
+++ dfs-datastores/src/main/java/com/backtype/hadoop/Consolidator.java
@@ -160,14 +160,20 @@ public class Consolidator {
             //because path is selected with a UUID
             if(!fs.exists(finalFile)) {
                 Path tmpFile = new Path("/tmp/consolidator/" + UUID.randomUUID().toString());
-                fs.mkdirs(tmpFile.getParent());
+                if (!fs.getClass().getName().equals("com.amazon.ws.emr.hadoop.fs.EmrFileSystem")) {
+                	fs.mkdirs(tmpFile.getParent());
+        		}
+
 
                 String status = "Consolidating " + sources.size() + " files into " + tmpFile.toString();
                 LOG.info(status);
                 rprtr.setStatus(status);
 
                 RecordStreamFactory fact = args.streams;
-                fs.mkdirs(finalFile.getParent());
+                if (!fs.getClass().getName().equals("com.amazon.ws.emr.hadoop.fs.EmrFileSystem")) {
+                	fs.mkdirs(finalFile.getParent());
+        		}
+
 
                 RecordOutputStream os = fact.getOutputStream(fs, tmpFile);
                 for(Path i: sources) {
