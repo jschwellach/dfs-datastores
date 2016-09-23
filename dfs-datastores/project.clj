2645003e6b9e375c6b86fcb8996fb581aec2f2d9
Janos Schwellach <janos.schwellach@helixleisure.com>
2016-09-23 11:11:14 +0800

    added pom file as im not able to compile with leiningen anymore

--- dfs-datastores/project.clj
+++ dfs-datastores/project.clj
@@ -29,16 +29,17 @@
                   [:name "Sam Ritchie"]
                   [:url "http://twitter.com/sritchie"]]]
   :javac-options ["-source" "1.8" "-target" "1.8"]
-  :java-source-paths ["src/main/java" "src/test/java"]
+  :java-source-paths ["src/main/java"]
   :junit ["src/test/java"]
   :profiles {:dev
              {:dependencies [[org.slf4j/slf4j-log4j12 "1.6.6"]]
-              :plugins [[lein-junit "1.1.5"]]}
+              :plugins [[lein-junit "1.1.8"]]}
              :provided
              {:dependencies [[org.apache.hadoop/hadoop-common "2.7.3"]
                              [org.apache.spark/spark-core_2.11 "2.0.0"]]}
              :test
-             {:dependencies [[junit/junit "4.12"]]}}
+             {:dependencies [[junit/junit "4.12"]]}
+             }
   :classifiers {:javadoc {:java-source-paths ^:replace []
                           :source-paths ^:replace []
                           :resource-paths ^:replace []}
